135532694b1641c3cda2610f410322b7c4d3ac85
Giannis Terzopoulos <giannis@pegneon.com>
2018-12-28 00:53:26 +0200

    changed domains

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domains=( "djangodemo3.ga" "www.djangodemo3.ga")
+domains=( "djangodemo.tk" "www.djangodemo.tk")
 rsa_key_size=4096
 data_path="./certbot"
 email="terzo.giannis@gmail.com"  #Adding a valid address is strongly recommended 
