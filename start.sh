e105abb533b27c6806f079008ac6851c3830dbfa
Giannis Terzopoulos <giannis@pegneon.com>
2018-12-28 00:18:14 +0200

    updated nginx.conf

--- start.sh
+++ start.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
-#
-#see https://semaphoreci.com/community/tutorials/dockerizing-a-python-django-web-application
-#but also the inclusion of cd to project name in start.sh as mentioned by rock in comments is important
-#This also means that you run the start script from root of project not inside app
-#ie [projectroot]/bin/bash start.sh not [approot]../start.sh
 
-# Start Gunicorn processes
-# workers which are the number of threads that will handle the requests coming into your application
-# Gunicorn recommends this value to be set at (2 x $num_cores) + 1.
-# You can read more on configuration of Gunicorn in their documentation.
-# 
 echo Starting Gunicorn.
 
-#make sure we are at project root /usr/src/app
 cd $PROJECT_ROOT
 
 if [ ! -f $PROJECT_ROOT/.build ]; then
@@ -23,8 +11,6 @@ if [ ! -f $PROJECT_ROOT/.build ]; then
   date > $PROJECT_ROOT/.build
 fi
 
-# Difference from article
-# CD to project as per comment above
 cd foobar
 exec gunicorn foobar.wsgi:application \
     --bind 0.0.0.0:8000 \
